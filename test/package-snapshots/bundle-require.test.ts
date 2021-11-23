e27ce91b286a5978ce2777faa4fa98744b666c16
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2021-11-23 14:19:38 +0100

    test: add tests for packages linking to jsdocs.io

---
+++ test/package-snapshots/bundle-require.test.ts
@@ -0,0 +1,9 @@
+import { expectPackageAPIToMatchSnapshot } from '../helpers/expect-package-api-to-match-snapshot';
+
+describe('bundle-require', () => {
+    const name = 'bundle-require';
+
+    it('2.1.2', async () => {
+        await expectPackageAPIToMatchSnapshot({ name, version: '2.1.2' });
+    });
+});
---
+++ test/package-snapshots/h3.test.ts
@@ -0,0 +1,9 @@
+import { expectPackageAPIToMatchSnapshot } from '../helpers/expect-package-api-to-match-snapshot';
+
+describe('h3', () => {
+    const name = 'h3';
+
+    it('0.3.3', async () => {
+        await expectPackageAPIToMatchSnapshot({ name, version: '0.3.3' });
+    });
+});
