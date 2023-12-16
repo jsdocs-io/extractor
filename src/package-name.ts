ea01c5c287e60c681daab53699496b35a3fc2ca7
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-12-16 18:50:18 +0100

    refactor: update return type

--- src/package-name.ts
+++ src/package-name.ts
@@ -2,7 +2,9 @@ import { Result, err, ok } from "neverthrow";
 import validate from "validate-npm-package-name";
 import { InvalidPackageNameError } from "./errors";
 
-export const packageName = (pkg: string): Result<string, Error> => {
+export const packageName = (
+  pkg: string,
+): Result<string, InvalidPackageNameError> => {
   const versionMarker = pkg.lastIndexOf("@");
   const name = pkg.slice(0, versionMarker > 0 ? versionMarker : undefined);
   if (validate(name).validForNewPackages) {
