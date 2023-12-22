9440f3a7f845b8c73e5504aa7d7a9830a9383bc9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-12-22 18:14:37 +0100

    refactor: add return type

--- src/package-json.ts
+++ src/package-json.ts
@@ -1,8 +1,10 @@
 import { ResultAsync } from "neverthrow";
-import { readPackage } from "read-pkg";
+import { readPackage, type NormalizedPackageJson } from "read-pkg";
 import { PackageJsonError } from "./errors";
 
-export const packageJson = (cwd: string) =>
+export const packageJson = (
+  cwd: string,
+): ResultAsync<NormalizedPackageJson, PackageJsonError> =>
   ResultAsync.fromPromise(
     readPackage({ cwd }),
     (e) => new PackageJsonError("failed to read package.json", { cause: e }),
