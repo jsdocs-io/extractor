8c476469506a4a187306a95470c3d5682b016c9b
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-12-24 02:30:37 +0100

    feat: add packageDeclarations

---
+++ src/package-declarations.ts
@@ -0,0 +1,20 @@
+import type { Project, SourceFile } from "ts-morph";
+import { containerDeclarations } from "./container-declarations";
+
+export type PackageDeclarationsOptions = {
+  project: Project;
+  indexFile: SourceFile;
+  maxDepth: number;
+};
+
+export const packageDeclarations = ({
+  project,
+  indexFile,
+  maxDepth,
+}: PackageDeclarationsOptions) =>
+  containerDeclarations({
+    project,
+    container: indexFile,
+    containerName: "",
+    maxDepth,
+  });
