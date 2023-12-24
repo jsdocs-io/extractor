807e5c8f471c8b6a83d9b16508df2401b0fc7431
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-12-24 01:06:11 +0100

    feat: add isInternalNode, ported from isInternalDeclaration

---
+++ src/is-internal-node.ts
@@ -0,0 +1,17 @@
+import { Node, SyntaxKind } from "ts-morph";
+import { nodeDocs } from "./node-docs";
+import { parseDocComment } from "./parse-doc-comment";
+
+export const isInternalNode = (node: Node, name = ""): boolean =>
+  name.startsWith("_") ||
+  name.startsWith("#") ||
+  hasPrivateModifier(node) ||
+  hasDocWithInternalTag(node);
+
+const hasPrivateModifier = (node: Node): boolean =>
+  Node.isModifierable(node) && node.hasModifier(SyntaxKind.PrivateKeyword);
+
+const hasDocWithInternalTag = (node: Node): boolean =>
+  nodeDocs(node).some((doc) =>
+    parseDocComment(doc).modifierTagSet.isInternal(),
+  );
