3b0e6b1539ad8609b71771311ff0b93aa3606d9c
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-12-24 02:17:21 +0100

    feat: add nodeIsExportedDeclarations, ported from isExportedDeclarations

---
+++ src/node-is-exported-declarations.ts
@@ -0,0 +1,13 @@
+import { Node, type ExportedDeclarations } from "ts-morph";
+
+export const nodeIsExportedDeclarations = (
+  node: Node,
+): node is ExportedDeclarations =>
+  Node.isVariableDeclaration(node) ||
+  Node.isFunctionDeclaration(node) ||
+  Node.isClassDeclaration(node) ||
+  Node.isInterfaceDeclaration(node) ||
+  Node.isEnumDeclaration(node) ||
+  Node.isTypeAliasDeclaration(node) ||
+  Node.isModuleDeclaration(node) ||
+  Node.isExpression(node);
