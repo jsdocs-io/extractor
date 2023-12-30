2878664d7c5ff27f18466be6a9847bb0093841b3
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-12-30 02:00:16 +0100

    feat: update ambientModulesDeclarations

--- src/ambient-modules-declarations.ts
+++ src/ambient-modules-declarations.ts
@@ -1,27 +1,39 @@
-import { Node, type Project } from "ts-morph";
+import { ModuleDeclaration, Node, type Project } from "ts-morph";
+import { isHidden } from "./is-hidden";
 
-export const ambientModulesDeclarations = (project: Project) => {
+export type AmbientModulesDeclarationsReturn = {
+  containerName: string;
+  exportName: string;
+  declaration: ModuleDeclaration;
+}[];
+
+export const ambientModulesDeclarations = (
+  project: Project,
+  containerName: string,
+): AmbientModulesDeclarationsReturn => {
   const ambientModulesDeclarations = [];
   for (const symbol of project.getAmbientModules()) {
     for (const declaration of symbol.getDeclarations()) {
+      if (isHidden(declaration) || !Node.isModuleDeclaration(declaration)) {
+        continue;
+      }
+      // TODO: add only from analyzed package
       const filepath = declaration.getSourceFile().getFilePath();
-      if (
-        !Node.isModuleDeclaration(declaration) ||
-        // TODO:
-        filepath.startsWith("/node_modules")
-      ) {
+      if (filepath.startsWith("/node_modules")) {
         continue;
       }
       const exportName = declaration.getName();
-
       // TODO:
       // Remove surrounding quotes and eventual spaces
-      const exportID = exportName
-        .replace(/"|'/g, "")
-        .replace(/\s/g, "_")
-        .trim();
-
-      ambientModulesDeclarations.push({ exportName, declaration });
+      // const exportID = exportName
+      //   .replace(/"|'/g, "")
+      //   .replace(/\s/g, "_")
+      //   .trim();
+      ambientModulesDeclarations.push({
+        containerName,
+        exportName,
+        declaration,
+      });
     }
   }
   return ambientModulesDeclarations;
