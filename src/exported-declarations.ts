1145fbce86b4047577da76faae3ed12a4e36662d
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-12-30 01:50:05 +0100

    feat: update exportedDeclarations

--- src/exported-declarations.ts
+++ src/exported-declarations.ts
@@ -1,17 +1,28 @@
-import type { ModuleDeclaration, SourceFile } from "ts-morph";
+import type {
+  ExportedDeclarations,
+  ModuleDeclaration,
+  SourceFile,
+} from "ts-morph";
+import { isExportedDeclarations } from "./is-exported-declarations";
 import { isHidden } from "./is-hidden";
+
+export type ExportedDeclarationsReturn = {
+  containerName: string;
+  exportName: string;
+  declaration: ExportedDeclarations;
+}[];
 
 export const exportedDeclarations = (
   container: SourceFile | ModuleDeclaration,
   containerName: string,
-) => {
+): ExportedDeclarationsReturn => {
   const exportedDeclarations = [];
   for (const [
     exportName,
     declarations,
   ] of container.getExportedDeclarations()) {
     for (const declaration of declarations) {
-      if (isHidden(declaration)) {
+      if (isHidden(declaration) || !isExportedDeclarations(declaration)) {
         continue;
       }
       exportedDeclarations.push({ containerName, exportName, declaration });
