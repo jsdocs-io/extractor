b3d4f62f7fd8e6173827c4502316b16defb719a4
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-01-05 00:54:17 +0100

    feat: add apparentType, ported from getApparentType

---
+++ src/apparent-type.ts
@@ -0,0 +1,17 @@
+import { TypeFormatFlags, ts, type Node } from "ts-morph";
+
+export const apparentType = (node: Node): string =>
+  // See:
+  // https://github.com/dsherret/ts-morph/issues/453#issuecomment-427405736
+  // https://github.com/dsherret/ts-morph/issues/453#issuecomment-667578386
+  node
+    .getType()
+    .getApparentType()
+    .getText(
+      node,
+      ts.TypeFormatFlags.NoTruncation |
+        TypeFormatFlags.UseAliasDefinedOutsideCurrentScope,
+    )
+    .replace(/^Number$/, "number")
+    .replace(/^Boolean$/, "boolean")
+    .replace(/^String$/, "string");
