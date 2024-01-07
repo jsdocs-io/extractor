02b69d0a62e4671bb5488e0bd04604f92d3c6199
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-01-07 19:08:34 +0100

    feat: add typeCheckerType, ported from getTypeChecker

---
+++ src/type-checker-type.ts
@@ -0,0 +1,16 @@
+import { TypeFormatFlags, ts, type Node } from "ts-morph";
+
+export const typeCheckerType = (node: Node): string => {
+  try {
+    const typeChecker = node.getProject().getTypeChecker().compilerObject;
+    const nodeType = typeChecker.getTypeAtLocation(node.compilerNode);
+    return typeChecker.typeToString(
+      nodeType,
+      node.compilerNode,
+      ts.TypeFormatFlags.NoTruncation |
+        TypeFormatFlags.UseAliasDefinedOutsideCurrentScope,
+    );
+  } catch {
+    return "any";
+  }
+};
