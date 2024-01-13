d33a5793854d794977b7200ca94d7e2d5f830f9e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-01-13 21:50:28 +0100

    feat: keep file path after `node_modules`

--- src/source-file-path.ts
+++ src/source-file-path.ts
@@ -1,5 +1,4 @@
 import type { Node } from "ts-morph";
 
-export const sourceFilePath = (node: Node): string => {
-  return node.getSourceFile().getFilePath();
-};
+export const sourceFilePath = (node: Node): string =>
+  node.getSourceFile().getFilePath().split("node_modules").pop()!;
