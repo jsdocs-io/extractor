a38ac2794a6bb42406d939ff984eca9191d3dd07
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-01-16 21:13:35 +0100

    test: add tests for isExportedDeclarations

---
+++ src/is-exported-declarations.test.ts
@@ -0,0 +1,48 @@
+import dedent from "ts-dedent";
+import {
+  ModuleKind,
+  ModuleResolutionKind,
+  Project,
+  ScriptTarget,
+} from "ts-morph";
+import { expect, test } from "vitest";
+import { isExportedDeclarations } from "./is-exported-declarations";
+
+test("is exported declarations", () => {
+  const project = new Project({
+    useInMemoryFileSystem: true,
+    compilerOptions: {
+      lib: ["lib.esnext.full.d.ts"],
+      target: ScriptTarget.ESNext,
+      module: ModuleKind.ESNext,
+      moduleResolution: ModuleResolutionKind.Bundler,
+    },
+  });
+  project.createSourceFile(
+    "file-module.ts",
+    dedent`
+    export {};
+    `,
+  );
+  const indexFile = project.createSourceFile(
+    "index.ts",
+    dedent`
+    export const fooVar: string;
+    export function fooFunc() {}
+    export class FooClass {}
+    export interface FooInterface {}
+    export enum FooEnum {}
+    export type FooType = {};
+    export namespace FooNamespace {}
+    export * as fooModule from './file-module';
+    export default 42;
+    `,
+  );
+  const exportedDeclarations = indexFile.getExportedDeclarations();
+  expect(exportedDeclarations.size).toBe(9);
+  for (const [, declarations] of exportedDeclarations) {
+    for (const declaration of declarations) {
+      expect(isExportedDeclarations(declaration)).toBe(true);
+    }
+  }
+});
