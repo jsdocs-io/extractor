ecc213df2ecf61e9b28956f3ab5b8a6bda42bd7f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-01-16 18:53:29 +0100

    test: add test for typeCheckerType

---
+++ src/type-checker-type.test.ts
@@ -0,0 +1,15 @@
+import { fromPartial } from "@total-typescript/shoehorn";
+import { expect, test } from "vitest";
+import { typeCheckerType } from "./type-checker-type";
+
+test("return `any` in case of errors", async () => {
+  expect(
+    typeCheckerType(
+      fromPartial({
+        getProject() {
+          throw new Error();
+        },
+      }),
+    ),
+  ).toBe("any");
+});
