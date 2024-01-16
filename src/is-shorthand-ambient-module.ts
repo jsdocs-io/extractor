da924876c7584c4bb636c0c790ee628ea31cad72
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-01-16 19:30:23 +0100

    docs: update comment on shorthand ambient modules name

--- src/is-shorthand-ambient-module.ts
+++ src/is-shorthand-ambient-module.ts
@@ -3,5 +3,6 @@ import { Node, type ModuleDeclaration } from "ts-morph";
 export const isShorthandAmbientModule = (
   node: Node,
 ): node is ModuleDeclaration =>
-  // Shorthand ambient modules have no body (e.g., `declare module 'foo';`).
+  // Shorthand ambient modules have no body (e.g., `declare module 'foo';`)
+  // and their name includes the quotes (e.g., name === "'foo'").
   Node.isModuleDeclaration(node) && !node.hasBody();
