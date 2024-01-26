5514ed7fb5e06674c446f5a4a4829abbf6d2ae48
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-01-26 20:08:03 +0100

    test: update error tests

--- src/errors.test.ts
+++ src/errors.test.ts
@@ -1,5 +1,14 @@
 import { expect, test } from "vitest";
-import { FsError, OsError } from "./errors";
+import {
+  FsError,
+  InstallPackageError,
+  OsError,
+  PackageDeclarationsError,
+  PackageJsonError,
+  PackageNameError,
+  PackageTypesError,
+  ProjectError,
+} from "./errors";
 
 test("os error", () => {
   expect(() => {
@@ -10,5 +19,41 @@ test("os error", () => {
 test("fs error", () => {
   expect(() => {
     throw new FsError("test");
+  }).toThrow();
+});
+
+test("install package error", () => {
+  expect(() => {
+    throw new InstallPackageError("test");
+  }).toThrow();
+});
+
+test("package name error", () => {
+  expect(() => {
+    throw new PackageNameError("test");
+  }).toThrow();
+});
+
+test("package json error", () => {
+  expect(() => {
+    throw new PackageJsonError("test");
+  }).toThrow();
+});
+
+test("package types error", () => {
+  expect(() => {
+    throw new PackageTypesError("test");
+  }).toThrow();
+});
+
+test("package declarations error", () => {
+  expect(() => {
+    throw new PackageDeclarationsError("test");
+  }).toThrow();
+});
+
+test("project error", () => {
+  expect(() => {
+    throw new ProjectError("test");
   }).toThrow();
 });
