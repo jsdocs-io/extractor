52b1a94fa216109af1ca894171ec10156c476ebb
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-01-27 19:41:50 +0100

    test: add test for AllExtractedDeclaration

---
+++ src/all-extracted-declaration.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "vitest";
+import * as allExtractedDeclaration from "./all-extracted-declaration";
+
+test("ok", () => {
+  const declaration: allExtractedDeclaration.AllExtractedDeclaration = {
+    kind: "variable",
+    id: "",
+    name: "",
+    docs: [],
+    file: "",
+    line: 0,
+    signature: "",
+  };
+  expect(declaration.kind).toBe("variable");
+});
