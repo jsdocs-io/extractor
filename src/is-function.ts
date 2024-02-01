09a435877299573b4fc88922ccc90684803a15d9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-02-01 18:22:47 +0100

    feat: don't check if function node is ambient or declaration

--- src/is-function.ts
+++ src/is-function.ts
@@ -1,6 +1,4 @@
 import { Node, type FunctionDeclaration } from "ts-morph";
 
 export const isFunction = (node: Node): node is FunctionDeclaration =>
-  (Node.isFunctionDeclaration(node) &&
-    (node.isAmbient() || node.isImplementation())) ||
-  Node.isArrowFunction(node);
+  Node.isFunctionDeclaration(node) || Node.isArrowFunction(node);
