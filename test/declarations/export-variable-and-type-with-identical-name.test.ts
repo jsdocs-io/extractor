5f88bf161402cd170c3a3cb2dedf8d6a9b8f9dd4
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-02-02 01:00:24 +0100

    test: add test for exports with identical name

---
+++ test/declarations/export-variable-and-type-with-identical-name.test.ts
@@ -0,0 +1,37 @@
+import dedent from "ts-dedent";
+import {
+  ModuleKind,
+  ModuleResolutionKind,
+  Project,
+  ScriptTarget,
+} from "ts-morph";
+import { expect, test } from "vitest";
+import { extractDeclarations } from "../../src";
+
+test("export variable and type with identical name", async () => {
+  const project = new Project({
+    useInMemoryFileSystem: true,
+    compilerOptions: {
+      lib: ["lib.esnext.full.d.ts"],
+      target: ScriptTarget.ESNext,
+      module: ModuleKind.ESNext,
+      moduleResolution: ModuleResolutionKind.Bundler,
+    },
+  });
+  const indexFile = project.createSourceFile(
+    "index.ts",
+    dedent`
+    // Similar to usage suggested with zod type inference.
+    export type foo = string | number
+    export const foo = { a: 1 };
+    `,
+  );
+  expect(
+    await extractDeclarations({
+      containerName: "",
+      container: indexFile,
+      maxDepth: 5,
+      project,
+    }),
+  ).toMatchSnapshot();
+});
