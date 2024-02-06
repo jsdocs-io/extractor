5c377a89d8fbf66fc99d68e8b326ded2441ceb94
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-02-06 19:45:41 +0100

    feat: export extracted declaration types

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,35 @@ export {
   type ExtractorError,
 } from "./errors";
 export {
+  type ExtractedClass,
+  type ExtractedClassConstructor,
+  type ExtractedClassMethod,
+  type ExtractedClassProperty,
+} from "./extract-class";
+export {
   extractDeclarations,
   type ExtractDeclarationsOptions,
   type ExtractedDeclaration,
   type ExtractedDeclarationKind,
 } from "./extract-declarations";
+export { type ExtractedEnum, type ExtractedEnumMember } from "./extract-enum";
+export { type ExtractedFunction } from "./extract-function";
+export {
+  type ExtractedInterface,
+  type ExtractedInterfaceCallSignature,
+  type ExtractedInterfaceConstructSignature,
+  type ExtractedInterfaceGetAccessor,
+  type ExtractedInterfaceIndexSignature,
+  type ExtractedInterfaceMethod,
+  type ExtractedInterfaceProperty,
+  type ExtractedInterfaceSetAccessor,
+} from "./extract-interface";
+export { type ExtractedNamespace } from "./extract-namespace";
 export {
   extractPackageApi,
   type ExtractPackageApiOptions,
 } from "./extract-package-api";
+export { type ExtractedTypeAlias } from "./extract-type-alias";
+export { type ExtractedVariable } from "./extract-variable";
 export { packageTypes } from "./package-types";
 export { parseDocComment } from "./parse-doc-comment";
