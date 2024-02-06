5fc9a532b29608c42f2b2930dc7e0cc13bb73dd2
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-02-06 19:47:24 +0100

    docs: use permalink

--- src/install-package.ts
+++ src/install-package.ts
@@ -12,7 +12,7 @@ export const installPackage = (
     // With verbose output on, bun prints one line per installed package
     // (for example, "foo@1.0.0"), including all installed dependencies.
     // These lines are between the two delimiting lines found here:
-    // https://github.com/oven-sh/bun/blob/main/src/install/lockfile.zig#L5354-L5355.
+    // https://github.com/oven-sh/bun/blob/972a7b7080bd3066b54dcb43e9c91c5dfa26a69c/src/install/lockfile.zig#L5369-L5370.
     const lines = stdout.split("\n");
     const beginHash = lines.findIndex((line) =>
       line.startsWith("-- BEGIN SHA512/256"),
