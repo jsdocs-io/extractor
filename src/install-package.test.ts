bc2562e5f65961a2e9a2585591f2ed5df34f6b49
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-02-10 00:17:25 +0100

    test: don't check for dynamic properties

--- src/install-package.test.ts
+++ src/install-package.test.ts
@@ -25,24 +25,7 @@ test("package with some production dependencies", async () => {
   await temporaryDirectoryTask(async (dir) => {
     process.chdir(dir);
     const installedPackages = await installPackage("query-registry@2.6.0");
-    expect(installedPackages).toStrictEqual(
-      ok([
-        "builtins@5.0.1",
-        "isomorphic-unfetch@3.1.0",
-        "lru-cache@6.0.0",
-        "make-error@1.3.6",
-        "node-fetch@2.7.0",
-        "query-registry@2.6.0",
-        "semver@7.6.0",
-        "tiny-lru@8.0.2",
-        "tr46@0.0.3",
-        "unfetch@4.2.0",
-        "url-join@4.0.1",
-        "validate-npm-package-name@4.0.0",
-        "webidl-conversions@3.0.1",
-        "whatwg-url@5.0.0",
-        "yallist@4.0.0",
-      ]),
-    );
+    expect(installedPackages.isOk()).toBe(true);
+    expect(installedPackages._unsafeUnwrap()).toContain("query-registry@2.6.0");
   });
 });
