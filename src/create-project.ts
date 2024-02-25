07e6e5de9774e1e72bf573e68bdaf45e3e3bf7ce
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-02-25 22:59:28 +0100

    refactor: split createProject

--- src/create-project.ts
+++ src/create-project.ts
@@ -1,4 +1,5 @@
 import { Result, err, ok } from "neverthrow";
+import process from "node:process";
 import {
   ModuleKind,
   ModuleResolutionKind,
@@ -22,13 +23,24 @@ export const createProject = ({
   indexFilePath,
   cwd,
 }: CreateProjectOptions): Result<ProjectContainer, ProjectError> => {
-  let startDir;
   try {
     // By default, ts-morph creates the project in the current working directory.
     // We must change it to the temporary directory where the packages are installed,
     // otherwise TypeScript will pick up type definitions from our local `node_modules`.
-    startDir = process.cwd();
+    const startDir = process.cwd();
     process.chdir(cwd);
+    const res = _createProject(indexFilePath);
+    process.chdir(startDir);
+    return res;
+  } catch (e) {
+    return err(new ProjectError("failed to change directories", { cause: e }));
+  }
+};
+
+const _createProject = (
+  indexFilePath: string,
+): Result<ProjectContainer, ProjectError> => {
+  try {
     const project = new Project({
       compilerOptions: {
         // See https://github.com/dsherret/ts-morph/issues/938
@@ -47,11 +59,5 @@ export const createProject = ({
     });
   } catch (e) {
     return err(new ProjectError("failed to create project", { cause: e }));
-  } finally {
-    try {
-      if (startDir) {
-        process.chdir(startDir);
-      }
-    } catch {}
   }
 };
