a12537dc13b9710993e58e49d74f8b6dd50733ee
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-27 00:51:47 +0200

    test: add query-registry@3.0.0 test

--- test/packages/query-registry.test.ts
+++ test/packages/query-registry.test.ts
@@ -12,3 +12,15 @@ test("query-registry@2.6.0", async () => {
 		packages: expect.any(Array),
 	});
 });
+
+test("query-registry@3.0.0", async () => {
+	expect(
+		await extractPackageApi({
+			pkg: "query-registry@3.0.0",
+		}),
+	).toMatchSnapshot({
+		analyzedAt: expect.any(String),
+		analyzedIn: expect.any(Number),
+		packages: expect.any(Array),
+	});
+});
