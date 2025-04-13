89bc8d1d1b24f5c5c61d5005b914a14aa13c23d9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-04-14 01:42:00 +0200

    test: update vitest config
    Pool forks is the default since vitest v2.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		// Prevent vitest from hanging when tests are done.
-		// See https://github.com/vitest-dev/vitest/issues/4526#issuecomment-1817817409.
-		pool: "forks",
-
-		// Package analysis tests require a lot of time.
-		testTimeout: 60000,
-
-		coverage: {
-			include: ["src/**"],
-		},
+		testTimeout: 60_000, // Analyzing npm packages takes time.
+		coverage: { include: ["src/**"] },
 	},
 });
