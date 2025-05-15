7aaaa7ef0e117d118fa63099fbcdc00536a023bb
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-05-16 01:25:42 +0200

    test: use pool threads to mitigate onTaskUpdate error

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
+		pool: "threads",
 		testTimeout: 60_000, // Analyzing npm packages takes time.
 		coverage: { include: ["src/**"] },
 	},
