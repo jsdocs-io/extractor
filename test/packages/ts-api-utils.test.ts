ed9e498bfbde5ae9786ac8174e5b084f1e75cd93
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-05-16 01:25:21 +0200

    test: add ts-api-utils@2.1.0 package

---
+++ test/packages/ts-api-utils.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "vitest";
+import { extractPackageApi } from "../../src";
+
+test("ts-api-utils@2.1.0", async () => {
+	expect(
+		await extractPackageApi({
+			pkg: "ts-api-utils@2.1.0",
+		}),
+	).toMatchSnapshot({
+		analyzedAt: expect.any(String),
+		analyzedIn: expect.any(Number),
+		packages: expect.any(Array),
+	});
+});
