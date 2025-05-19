7c29d514db184311e4760f4ad7bf84360566b11c
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-05-19 17:06:47 +0200

    test: add tests for the npm and @types/npm packages
    See https://github.com/jsdocs-io/web/issues/574

---
+++ test/packages/npm.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "vitest";
+import { extractPackageApi } from "../../src";
+
+test("npm@11.4.0", async () => {
+	await expect(
+		extractPackageApi({
+			pkg: "npm@11.4.0",
+		}),
+	).rejects.toThrow();
+});
+
+test("@types/npm@7.19.3", async () => {
+	expect(
+		await extractPackageApi({
+			pkg: "@types/npm@7.19.3",
+		}),
+	).toMatchSnapshot({
+		analyzedAt: expect.any(String),
+		analyzedIn: expect.any(Number),
+		packages: expect.any(Array),
+	});
+});
