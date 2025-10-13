34815d6a233ee95a7dac317d602ae94306027c8e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-10-13 20:21:01 +0200

    feat: update exports from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
+export { bunPackageManager } from "./bun-package-manager.ts";
 export * from "./errors.ts";
-export * from "./extract-package-api.ts";
+export { extractPackageApi, extractPackageApiEffect } from "./extract-package-api.ts";
+export { packageJson } from "./package-json.ts";
+export { PackageManager } from "./package-manager.ts";
+export { packageName } from "./package-name.ts";
+export { packageTypes } from "./package-types.ts";
+export { parseDocComment } from "./parse-doc-comment.ts";
 export type * from "./types.ts";
+export { workDir } from "./work-dir.ts";
 
-// TODO
-// export { bunPackageManager } from "./bun-package-manager.ts";
-// export { ProjectError } from "./create-project.ts";
 // export { extractDeclarations } from "./extract-declarations.ts";
-// export { PackageDeclarationsError } from "./package-declarations.ts";
-// export { PackageJsonError, packageJson } from "./package-json.ts";
-// export {
-// 	InstallPackageError,
-// 	PackageManager,
-// 	type InstallPackageOptions,
-// } from "./package-manager.ts";
-// export { PackageNameError, packageName } from "./package-name.ts";
-// export { PackageTypesError, packageTypes } from "./package-types.ts";
-// export { parseDocComment } from "./parse-doc-comment.ts";
-// export { WorkDirError, workDir, type WorkDir } from "./work-dir.ts";
