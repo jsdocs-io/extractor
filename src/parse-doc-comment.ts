1c5d2c6195ec01611026f691a1792638962ff571
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-10-13 19:12:59 +0200

    refactor: update parseDocComment

--- src/parse-doc-comment.ts
+++ src/parse-doc-comment.ts
@@ -4,13 +4,12 @@ import memoize from "memoize";
 /**
 `parseDocComment` parses a JSDoc comment using `@microsoft/tsdoc`.
 
+@remarks
 Parsed comments are memoized.
 
-@param s - the raw string comment
-
-@internal
+@param comment - the raw string comment
 */
-export const parseDocComment = memoize((s: string): DocComment => {
+export const parseDocComment = memoize((comment: string): DocComment => {
 	const parser = new TSDocParser();
-	return parser.parseString(s).docComment;
+	return parser.parseString(comment).docComment;
 });
