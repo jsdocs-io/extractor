c30a4734745b2eb3c36efdbfbd235f73dfd53ca2
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-10-14 22:37:28 +0200

    refactor: update exportEqualsDeclarations

--- src/export-equals-declarations.ts
+++ src/export-equals-declarations.ts
@@ -1,38 +1,27 @@
-import { ModuleDeclaration, SourceFile, SyntaxKind, type ExportedDeclarations } from "ts-morph";
+import { ModuleDeclaration, SourceFile, SyntaxKind } from "ts-morph";
 import { isNamespace } from "./declaration-type-guards.ts";
 import { isExportedDeclarations } from "./is-exported-declarations.ts";
 import { isHidden } from "./is-hidden.ts";
 import { isShorthandAmbientModule } from "./is-shorthand-ambient-module.ts";
-
-export type ExportEqualsDeclarationsReturn = {
-	containerName: string;
-	exportName: string;
-	declaration: Exclude<ExportedDeclarations, ModuleDeclaration>;
-}[];
+import type { FoundDeclaration } from "./types.ts";
 
 export function exportEqualsDeclarations(
 	containerName: string,
 	container: SourceFile | ModuleDeclaration,
-): ExportEqualsDeclarationsReturn {
-	if (isShorthandAmbientModule(container)) {
-		// Export equals declarations may exist inside the body of ambient modules.
-		// However, this is impossible for shorthand ambient modules with no body.
-		return [];
-	}
+): FoundDeclaration[] {
+	// Shorthand ambient modules have no body and thus no declarations.
+	if (isShorthandAmbientModule(container)) return [];
 	const exportIdentifier = container
 		.getExportAssignment((assignment) => assignment.isExportEquals())
 		?.getLastChildByKind(SyntaxKind.Identifier);
-	if (!exportIdentifier) {
-		return [];
-	}
+	if (!exportIdentifier) return [];
 	const exportName = exportIdentifier.getText();
 	const exportEqualsDeclarations = [];
 	for (const declaration of exportIdentifier.getDefinitionNodes()) {
-		if (isHidden(declaration) || !isExportedDeclarations(declaration)) {
-			continue;
-		}
+		if (!isExportedDeclarations(declaration)) continue;
+		if (isHidden(declaration)) continue;
 		if (isNamespace(declaration)) {
-			// Skip namespaces since `exportedDeclarations()` already extracts
+			// Ignore namespaces since `exportedDeclarations()` already extracts
 			// the inner declarations of an export equals namespace as
 			// non-namespaced declarations belonging to the parent container.
 			// See snapshot for `export-equals-function-and-namespace.test.ts`.
