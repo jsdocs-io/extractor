78aba67d815a4dd2133198a533c1753db161f2e3
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-10-28 18:21:13 +0100

    test: use threads pool

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		pool: "vmThreads",
+		pool: "threads",
 		testTimeout: 60_000, // Analyzing npm packages takes time.
 		coverage: { include: ["src/**"] },
 	},
